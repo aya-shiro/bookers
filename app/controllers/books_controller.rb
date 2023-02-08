d160836d65f47fe4530447aef533bfed424f7a55
EC2 Default User <ec2-user@ip-172-31-7-225.ap-northeast-1.compute.internal>
2023-02-08 11:35:30 +0000

    [test]bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,10 +32,13 @@ protect_from_forgery
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully updated!"
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated!"
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
